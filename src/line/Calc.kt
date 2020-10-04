7acc675ef8825c7a7c69332aa4bd50efcf08ae3d
SketchySketch <xzl2628655535@163.com>
2020-10-04 22:10:22 +0800

    Emergency fix!

--- src/line/Calc.kt
+++ src/line/Calc.kt
@@ -1,6 +1,7 @@
 package line
 
 import kotlin.math.*
+import angle.*
 
 fun nSector(A: Point, B: Point, n: Int): MutableList<Point> {
   val deltaX = A.x - B.x
@@ -15,20 +16,39 @@ fun nSector(A: Point, B: Point, n: Int): MutableList<Point> {
   return ls
 }
 
-fun rotate(A: Point, O: Point, angle: Double): Point {
+fun rotate(A: Point, O: Point, angle: Rad): Point {
   val a = A.x
   val b = A.y
   val x = O.x
   val y = O.y
-  val theta = angle / 180 * PI
+  val theta = angle.degree
   return Point(
       cos(theta) * (a - x) - sin(theta) * (b - y) + x,
       sin(theta) * (a - x) + cos(theta) * (b - y) + y
   )
 }
 
-fun rotate(l: Line, O: Point, angle: Double): Line {
-  val theta = angle / 180 * PI
+fun rotate(l: Line, O: Point, angle: Rad): Line {
+  val theta = angle.degree
+  val k = tan(theta + atan(l.k))
+  val anchor = rotate(l.pointOnLineX(0.0), O, angle)
+  return Line(anchor, k)
+}
+
+fun rotate(A: Point, O: Point, angle: Deg): Point {
+  val a = A.x
+  val b = A.y
+  val x = O.x
+  val y = O.y
+  val theta = angle.rad.degree
+  return Point(
+      cos(theta) * (a - x) - sin(theta) * (b - y) + x,
+      sin(theta) * (a - x) + cos(theta) * (b - y) + y
+  )
+}
+
+fun rotate(l: Line, O: Point, angle: Deg): Line {
+  val theta = angle.rad.degree
   val k = tan(theta + atan(l.k))
   val anchor = rotate(l.pointOnLineX(0.0), O, angle)
   return Line(anchor, k)
